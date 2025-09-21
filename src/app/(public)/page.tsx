654b8ae628d3f5000b0774e34a69a6b7b0ee2b80
Sajmul <sajmul1427@gmail.com>
2025-09-21 23:55:46 +0600

    home data showing done

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,10 +1,21 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import BlogCard from "@/components/modules/Blogs/BlogCard";
 import Hero from "@/components/modules/Home/Hero";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/posts`);
+  const {data: blogs} = await res.json();
+
   return (
-    <div>
+    <div className="box">
       <Hero />
       <h2 className="text-center my-5 text-4xl">Featured Posts</h2>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {
+          blogs.slice(0,3).map((blog: any) => <BlogCard post={blog} key={blog._id} />)
+        }
+      </div>
     </div>
   );
 }
