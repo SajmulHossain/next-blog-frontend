fc2f7ac940c7e5860e0c1a729e780a55a6cf49b0
Sajmul <sajmul1427@gmail.com>
2025-09-21 23:59:19 +0600

    isr for home page blog  and limit for 3 data

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -3,8 +3,12 @@ import BlogCard from "@/components/modules/Blogs/BlogCard";
 import Hero from "@/components/modules/Home/Hero";
 
 export default async function HomePage() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/posts`);
-  const {data: blogs} = await res.json();
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/posts?limit=3`, {
+    next: {
+      revalidate: 40,
+    },
+  });
+  const { data: blogs } = await res.json();
 
   return (
     <div className="box">
@@ -12,9 +16,9 @@ export default async function HomePage() {
       <h2 className="text-center my-5 text-4xl">Featured Posts</h2>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {
-          blogs.slice(0,3).map((blog: any) => <BlogCard post={blog} key={blog._id} />)
-        }
+        {blogs.map((blog: any) => (
+          <BlogCard post={blog} key={blog._id} />
+        ))}
       </div>
     </div>
   );
