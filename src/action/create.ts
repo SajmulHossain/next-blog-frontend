74616ea0f3cbf2dc39f44fbd6bd5cd5aa909c254
Sajmul <sajmul1427@gmail.com>
2025-09-24 21:47:55 +0600

    data modify done

--- src/action/create.ts
+++ src/action/create.ts
@@ -3,9 +3,9 @@
 export const create = async(data: FormData) => {
     const info = Object.fromEntries(data.entries());
     const body = {
+        ...info,
         authorId: 1,
         tags: info.tags.toString().split(",").map(tag => tag.trim()),
-        ...info,
     }
 
     console.log(body);
